fa17900c8f4754a115bbcebb6604d67a12b00bfe
andres211085 <andresmartinsal@gmail.com>
2015-08-07 14:52:19 +0200

    Primera Subida

---
+++ Assets/Bola.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bola : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+
+	float jump=1.5f;
+
+
+	void Start () {
+	
+		rb2d = GetComponent<Rigidbody2D>();
+
+
+	}
+	
+
+	void Update () {
+	
+
+
+	}
+
+	void FixedUpdate(){
+		if(Input.GetButton("Jump")){	
+			Debug.Log("Jump");
+			rb2d.AddForce (new Vector2(0,jump));
+			//Rigidbody2D.AddForce (new Vector2(0,jump));
+		}
+	}
+}
